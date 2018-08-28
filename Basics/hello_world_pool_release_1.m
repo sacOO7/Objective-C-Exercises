46bad38a68e7b2cc31361de708e01e788405a748
sacOO7 <sachinshinde7676@gmail.com>
2018-08-28 17:23:36 +0530

    updated hello world examples, added manual release and auto release memory cycle examples

--- Basics/hello_world_pool_release_1.m
+++ Basics/hello_world_pool_release_1.m
@@ -1,6 +1,7 @@
 #include <Foundation/Foundation.h>
-
 int main (int argc, char ** argv) {
+        NSAutoreleasePool *autoreleasepool = [[NSAutoreleasePool alloc] init];
         NSLog(@"Hello world");
+        [autoreleasepool drain];
         return 0;
 }
