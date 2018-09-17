e44f1b0c3738e0b6f3d2059a2c1b4d88fa36d260
sacOO7 <sachinshinde7676@gmail.com>
2018-09-17 14:48:26 +0530

    added example for immutable dictionary

--- Advanced/Collections/nsDictionary.m
+++ Advanced/Collections/nsDictionary.m
@@ -1,4 +1,21 @@
 #include <Foundation/Foundation.h>
 int main() {
+    NSDictionary * nsDict = @{
+        @"Color" : @"Red",
+        @"City": @"Pune",
+        @"Nation" : @"India"
+    };
+
+    NSLog(@"Color is %@", nsDict[@"Color"]);
+    NSLog(@"City is %@", nsDict[@"City"]);
+    NSLog(@"Nation is %@", nsDict[@"Nation"]);
+
+
+    // Enumerating through keys and values
+
+    for (NSString * key in nsDict) {
+        id value = nsDict[key];
+        NSLog(@"Key is %@ and value is %@", key, value);
+    }
     
 }
