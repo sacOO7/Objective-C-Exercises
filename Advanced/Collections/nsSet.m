be5f26482bd2557ee34ee69bcf48b263cbc3e94e
sacOO7 <sachinshinde7676@gmail.com>
2018-09-17 15:28:03 +0530

    added values to NSSet

--- Advanced/Collections/nsSet.m
+++ Advanced/Collections/nsSet.m
@@ -1,4 +1,22 @@
 #include <Foundation/Foundation.h>
 int main() {
-    
+    // By DEFAULT IMMUTABLE SET IS CREATED, ALL ELEMENTS ARE UNIQUE, NOT NECESSARILY IN ORDER.
+    NSSet * set = [[NSSet alloc]init];
+    NSLog(@"Value of set is %@", set);
+
+    // Initializing with array
+    NSSet * setUsingArray = [NSSet setWithArray : @[@"Easy", @"Dude"]];
+    NSLog(@"Value of set is %@", setUsingArray);
+
+    //Initializing with object
+    NSSet * objectName = [NSSet setWithObject : @"My Name is Khan"];
+    NSLog(@"Value of set is %@", objectName);
+
+    // Initalizing with objects
+    NSSet * objectNames = [NSSet setWithObjects : @"My Name is Khan", @"Another name is Don", nil];
+    NSLog(@"Value of set is %@", objectNames);
+
+    return 0;
+
+
 }
