0f37125ab117cdbe94de583362e3873ab9e823fe
sacOO7 <sachinshinde7676@gmail.com>
2018-09-17 15:38:29 +0530

    updated example for mutableSet

--- Advanced/Collections/nsMutableSet.m
+++ Advanced/Collections/nsMutableSet.m
@@ -1,4 +1,19 @@
 #include <Foundation/Foundation.h>
 int main() {
-    
+    NSMutableSet * mutableSet = [[NSMutableSet alloc] init];
+
+    [mutableSet addObjectsFromArray : @[@"red", @"blue"]];
+     
+    NSLog(@"Set values are %@", mutableSet);
+
+    //Removing one value from the set
+    [mutableSet removeObject : @"blue"];
+    NSLog(@"Set values are %@", mutableSet);
+
+    //Adding new value to the set
+    [mutableSet addObject : @"green"];
+    NSLog(@"Set values are %@", mutableSet);
+
+
+
 }
